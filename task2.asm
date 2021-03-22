2cfbf52d625cb4e996b1b946c71541f2374422f7
Lina-Ras <70744770+Lina-Ras@users.noreply.github.com>
2021-03-22 09:00:36 +0300

    Update task2.asm

--- task2.asm
+++ task2.asm
@@ -3,16 +3,16 @@ PUBLIC _sum@16
 .model flat
 .code
 _sum@16 proc
-mov eax, [esp + 4]
-mov ebx, [esp + 8]
-mov ecx, [esp + 12]
-mov edx, [esp + 16]
+mov eax, dword ptr[esp + 4]
+mov ebx, dword ptr[esp + 8]
+mov ecx, dword ptr[esp + 12]
+mov edx, dword ptr[esp + 16]
 _start_mul:
 cmp edx, 0
     je _end_of_mul
-    mov esi, [eax]
-    imul esi, [ebx]
-    mov[ecx], esi
+    mov esi, dword ptr[eax]
+    imul esi, dword ptr[ebx]
+    mov dword ptr[ecx], esi
     add eax, 4
     add ebx, 4
     add ecx, 4
@@ -21,12 +21,12 @@ cmp edx, 0
     _end_of_mul:
 
 mov eax, 0
-mov ecx, [esp + 12]
-mov edx, [esp + 16]
+mov ecx, dword ptr[esp + 12]
+mov edx, dword ptr[esp + 16]
 _start_sum:
 cmp edx, 0
     je _end_of_sum
-    add eax, [ecx]
+    add eax, dword ptr[ecx]
     add ecx, 4
     sbb edx, 1
     jmp _start_sum
