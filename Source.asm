e27dadc588cf804fa8043c6e04b62093b511f88e
Lina Dva <Anhelina.Pashkowets@gmail.com>
2021-04-08 17:36:52 +0300

    I finally found my mistakes :)

---
+++ Source.asm
@@ -0,0 +1,116 @@
+.code
+
+RowCalc proc
+push rbp
+mov rbp, rsp
+mov rbx, [rbp + 24]
+mov r10, 0
+mov r9, [rbp + 16]
+	_begin_ch:
+	movzx eax, word ptr[rbx]
+	add r10, rax
+	sub r9, 1
+	add rbx, 2
+	cmp r9, 0
+	jne _begin_ch;
+mov rax, r10
+pop rbp
+ret 16
+RowCalc endp
+
+ColumnCalc proc
+push rbp
+mov rbp, rsp
+mov rcx, [rbp + 24]
+mov r10, 0
+mov r9, [rbp + 16]
+	_begin_cv:
+	mov rbx, qword ptr[rcx]
+	movzx eax, word ptr[rbx]
+	add r10, rax
+	sub r9, 1
+	add rcx, 8
+	cmp r9, 0
+	jne _begin_cv;
+mov rax, r10
+pop rbp
+ret 16
+ColumnCalc endp
+
+IsMagicSquare proc
+mov r8, rdx
+mov r12, rcx
+
+_begin_h:
+	mov rbx, qword ptr [rcx]
+		push rbx
+		push rdx
+		call RowCalc
+	cmp r8, rdx
+	jne _skiph;
+	mov r11, r10
+	_skiph:
+	cmp r11, r10
+	jne _endf;
+	sub r8, 1
+	add rcx, 8
+	cmp r8, 0
+jne _begin_h;
+
+mov r8, 0
+_begin_v:
+	mov rcx, r12
+		push rcx
+		push rdx
+		call ColumnCalc
+	cmp r11, r10
+	jne _endf;
+	add r8, 1
+	add rcx, 8
+	cmp r8, rdx
+jne _begin_v;
+
+mov r8, 0  ;нумар радку
+mov rcx, r12   
+mov r10, 0
+	_begin_dm:
+		mov rbx, qword ptr[rcx]
+		mov r15, r8
+		imul r15, 2
+		add rbx, r15
+		movzx eax, word ptr[rbx]
+		add r10, rax
+		inc r8
+		add rcx, 8
+		cmp r8, rdx
+	jne _begin_dm;
+cmp r11, r10
+jne _endf;
+
+mov r8, rdx  ;нумар радку
+dec r8
+mov rcx, r12   
+mov r10, 0
+	_begin_ds:
+		mov rbx, qword ptr[rcx]
+		mov r15, r8
+		imul r15, 2
+		add rbx, r15
+		movzx eax, word ptr[rbx]
+		add r10, rax
+		dec r8
+		add rcx, 8
+		cmp r8, -1
+	jne _begin_ds;
+cmp r11, r10
+jne _endf;
+
+mov rax, 1
+jmp _endt;
+_endf:
+mov rax, 0
+
+_endt:
+ret
+IsMagicSquare endp
+end
